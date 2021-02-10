5a939f0b9de75e7c73fc7fed1f23760abeb09087
Ifihanagbara Olusheye <victoriaolusheye@gmail.com>
2021-02-10 23:25:01 +0100

    adding of all numbers in a list

--- exercises.jl
+++ exercises.jl
@@ -1,4 +1,4 @@
-# 3
+# 1
 """Checks if the sum of two sides of a triangle is greater than one side."""
 function istriangle(x, y, z)
     parse(Int64, x); parse(Int64, y); parse(Int64, z)
@@ -23,3 +23,18 @@ function check()
 end
 
 check()
+
+# 2
+"""Adds all the numbers in a list"""
+function addall(numbers)
+    total = 0
+    parse(Int64, x)
+    for x in numbers
+        total += x
+    end
+end
+
+list_of_num = [34, 6, 8, 10]
+println(addall(list_of_num))
+
+# 3
